644822b570e6d38a2b43275d8d6d43a1423561a2
Victor Franco <vfrancohdez1@hotmail.com>
2022-12-04 12:09:52 -0600

    refactor: rename Body to Menu

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
 import React from 'react';
+import {Route, Routes} from 'react-router-dom';
 import './App.css';
 import Header from './components/Header.js';
-import Body from './components/Body.js';
-import {Route, Routes} from 'react-router-dom';
-
-const Home = () => <Body />
+import Menu from './components/Menu.js';
 
 function App() {
   return (
     <div className="App">
       <Header>E-Commerce</Header>
       <Routes>
-        <Route path='/' element={<Home/>}/>
+        <Route path='/' element={<Menu />}/>
       </Routes>
     </div>
   );
--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Card from './Card.js';
-import './Body.css';
+import './Menu.css';
 
 function Body () {
   return (
