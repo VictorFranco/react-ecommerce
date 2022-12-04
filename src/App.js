934442808c31752c8fcdb7368cbca68e7a0d68c4
Victor Franco <vfrancohdez1@hotmail.com>
2022-12-03 20:34:26 -0600

    feat(header): create header component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
+import React from 'react';
 import './App.css';
+import Header from './components/Header.js';
 
 function App() {
   return (
     <div className="App">
+      <Header>E-Commerce</Header>
     </div>
   );
 }
