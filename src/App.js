ef1d787173a44ca3e91e88bbbf9546d65f460096
Victor Franco <vfrancohdez1@hotmail.com>
2022-12-04 08:09:46 -0600

    feat(body): create body component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header.js';
+import Body from './components/Body.js';
 
 function App() {
   return (
     <div className="App">
       <Header>E-Commerce</Header>
+      <Body></Body>
     </div>
   );
 }
---
+++ src/components/Body.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Body () {
+  return (
+    <div>
+      <div>Captura de artículo</div>
+      <div>Compra de artículos</div>
+    </div>
+  );
+}
+
+export default Body;
