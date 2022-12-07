2d447d9c9d089cde38cf811a71bfe54ce721398b
Victor Franco <vfrancohdez1@hotmail.com>
2022-12-07 07:15:33 -0600

    feat(Modal): add close button

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -6,11 +6,15 @@ function Modal() {
   const context = useContext(Context)
   if (!context || Object.keys(context.product).length === 0)
     return <></>;
+  const CloseModal = () => {
+    context.setProduct({})
+  }
 
   const {nombre,descripcion,fotografia} = context.product
   return (
     <div className='modal'>
       <div>
+        <input className='close' type="submit" value="x" onClick={CloseModal}/>
         <img alt={nombre} src={fotografia} />
         <div className='info'>
           <div className='name'>{nombre}</div>
