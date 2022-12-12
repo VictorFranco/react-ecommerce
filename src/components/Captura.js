afa9957f8db678cdc57726c4707c567311476267
Victor Franco <vfrancohdez1@hotmail.com>
2022-12-11 21:14:43 -0600

    feat(Captura): send product information to server

--- src/components/Captura.js
+++ src/components/Captura.js
@@ -2,15 +2,41 @@ import React from 'react'
 import './Captura.css'
 import {Link} from 'react-router-dom'
 
+const AltaArticulo = data => {
+  fetch('http://172.177.11.192:8080/Ecommerce/rest/ws/alta_articulo', {
+    method: 'POST',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(data)
+  })
+  .then(({status}) => alert(status == 200 ? "OK" : "Error"))
+}
+
+const OnSubmit = e => {
+  e.preventDefault()
+
+  let form_data = new FormData(e.target)
+  let data = Object.fromEntries(form_data)
+  let reader = new FileReader();
+  reader.onloadend = () => {
+    data.fotografia = reader.result.split(',')[1]
+    data = {articulo: data}
+    AltaArticulo(data)
+  }
+  reader.readAsDataURL(data.fotografia);
+}
+
 function Captura () {
   return (
     <div className='body captura'>
-      <form>
-        <label>Nombre:</label><input type='text'/>
-        <label>Descripción:</label><input type='text'/>
-        <label>Precio:</label><input type='number'/>
-        <label>Cantidad:</label><input type='number'/>
-        <label>Fotografía:</label><input type='file'/>
+      <form onSubmit={OnSubmit}>
+        <label>Nombre:</label><input name='nombre' type='text'/>
+        <label>Descripción:</label><input name='descripcion' type='text'/>
+        <label>Precio:</label><input name='precio' type='number'/>
+        <label>Cantidad:</label><input name='cantidad' type='number'/>
+        <label>Fotografía:</label><input name='fotografia' type='file'/>
         <input type='submit' value="Enviar"/>
       </form>
       <Link to = "/" className='return'> Regresar </Link>
