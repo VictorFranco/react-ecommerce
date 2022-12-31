8a0868403b4a86b8bfebfe2fbd667f73760fcb3c
Victor Franco <vfrancohdez1@hotmail.com>
2022-12-30 21:20:15 -0600

    feat(Product): add API call to insert the product

--- src/components/Product.js
+++ src/components/Product.js
@@ -2,8 +2,24 @@ import React, {useState, useContext} from 'react'
 import './Product.css'
 import ProductContext from '../context/ProductContext.js'
 
+const AgregarACarrito = (nombre, cantidad) => {
+  const data = {nombre,cantidad}
+  fetch('http://172.177.11.192:8080/Ecommerce/rest/ws/agregar_a_carrito', {
+    method: 'POST',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(data)
+  })
+  .then(data => {
+    if (data.status === 200) alert("OK")
+    else data.json().then(({message}) => alert(message))
+  })
+}
+
 function Product({info}) {
-  const {nombre,precio,cantidad,fotografia} = info;
+  const {nombre,precio,fotografia} = info;
   const foto = `data:image/jpeg;base64,${fotografia}`
   const [counter, setCounter] = useState(1);
   const change_number = e => setCounter(e.target.value)
@@ -12,8 +28,10 @@ function Product({info}) {
   const OnSubmit = e => {
     e.preventDefault()
     let name = e.nativeEvent.submitter.value
+    let cantidad = e.target.cantidad.value
     switch (name) {
       case "Comprar":
+        AgregarACarrito(nombre,cantidad)
         break;
       case "Detalle":
         const {setProduct} = context
@@ -29,7 +47,7 @@ function Product({info}) {
       <form className='info' onSubmit={OnSubmit} >
         <div>{nombre}</div>
         <div>${precio}</div>
-        <input type="number" min="1" max={cantidad} value={counter} onChange={change_number} />
+        <input type="number" name="cantidad" min="1" value={counter} onChange={change_number} />
         <input type="submit" value="Comprar" />
         <input type="submit" value="Detalle" />
       </form>
